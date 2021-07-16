77de6f07daa1dea982d654e1aff55432de8485e2
whitehatjrafreennithin <78360402+whitehatjrafreennithin@users.noreply.github.com>
2021-07-16 16:56:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function setup() {
+    video = createCapture(VIDEO);
+video.size(550,500)
+canvas = createCanvas(550, 450);
+canvas.position(560, 100);
+
+
+poseNet = ml5.poseNet(video, modelLoaded)
+poseNet.on("pose", gotPoses)
+
+}
+
+function modelLoaded() {
+    console.log("Model Loaded")
+}
+
+function gotPoses(results) {
+    if(results.length>0){
+        console.log(results)
+    }
+}
+
+function draw() {
+    background("coral")
+}
